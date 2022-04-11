d14518f6b47d79cd3950d1ab5d2d1f7d472e1971
SolvedbySoftwareCo <chanida@solvedbysoftwareco.com>
2022-04-11 11:11:27 -0600

    Url issue

--- routes/activity.js
+++ routes/activity.js
@@ -118,7 +118,7 @@ exports.execute = function (req, res) {
 
     // FOR TESTING
     logData(req);
-    res.send(200, 'Publish');
+    res.send(200, 'Execute');
 
     // Used to decode JWT
     // JWT(req.body, process.env.jwtSecret, (err, decoded) => {
@@ -159,7 +159,7 @@ exports.publish = function (req, res) {
     // Data from the req and put it in an array accessible to the main app.
     //console.log( req.body );
 //     logData(req);
-//     res.send(200, 'Publish');
+     res.send(200, 'Publish');
 };
 
 /*
