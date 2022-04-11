c7ed4d769802a8e3b5a400b90a135a85b9fee934
SolvedbySoftwareCo <chanida@solvedbysoftwareco.com>
2022-04-11 01:01:47 -0600

    Url issue

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -100,7 +100,7 @@ define([
             "authToken": authToken,
             "messagingService": messagingService,
             "body": body,
-            "to": "{{Contact.Attribute.TestTwilio.phoNo}}" //<----This should map to your data extension name and phone number column
+            "to": "{{Contact.Attribute.TestTwilio.phno}}" //<----This should map to your data extension name and phone number column
         }];
 
         payload['metaData'].isConfigured = true;
